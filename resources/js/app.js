3fe0ab783899ca8ef4ac3437eacc76eecdf21b56
riveur <riveur.cpy@gmail.com>
2023-01-29 14:27:41 +0100

    add bulma js behaviors

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import '../css/app.css';
+
+import { initDeleteButton as initBulmaNotificationDeleteButton } from './BulmaBehaviors/BulmaNotification.js';
+import { initToggleButtons } from './BulmaBehaviors/ButtonToggle.js';
+
+initBulmaNotificationDeleteButton();
+initToggleButtons();
